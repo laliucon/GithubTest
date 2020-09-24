3fba23edb8ceb64afd6b84b57cf7de9daed61f6d
Hung Cun <hungcun1996@csu.fullerton.edu>
2020-09-24 14:49:05 -0700

    Initial Commit

---
+++ GithubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubTest
+//
+//  Created by Hung Cun on 9/24/20.
+//  Copyright © 2020 Hung Cun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
